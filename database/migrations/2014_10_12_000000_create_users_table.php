f7bb130f926a8d42e9ec896648d699322a7090f5
felipe cruz <felipephplow@gmail.com>
2021-10-20 11:31:10 -0300

    fsd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('permission')->default(0);
+            $table->integer('plan')->default(0);
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
